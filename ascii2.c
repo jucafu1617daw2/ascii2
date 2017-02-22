3d4dc8266ed358748d6394312b6d5f492ed9fe6e
Jucafu1617daw2 <jucafu1617daw2@gmail.com>
2017-02-22 20:33:49 +0100

    Commit 1 del projecte ascii2

---
+++ ascii2.c
@@ -0,0 +1,16 @@
+//Autor: Juan Jose Castillo Funes
+#include <stdio.h>
+
+int main() {
+	char c;
+	int n,i;
+
+	printf("TAULA ASCII MAJÚSCULES 22-2-2017\n");
+	printf("CAR  HEX  DEC\n");
+	n='Z'-'A';
+	for (i=n;i>=0;i--) {
+		c='A'+i;
+		printf("%c    %x   %d\n",c,c,c);
+	}
+	return(0);
+}
